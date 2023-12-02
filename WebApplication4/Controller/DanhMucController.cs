85e793cb55a9d6bb69a10456a8f0e680caccf2ec
tuanda24 <dat.2000.dz@gmial.com>
2023-12-02 16:15:10 +0700

    Add project files.

---
+++ WebApplication4/Controller/DanhMucController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication4.Controller
+{
+    public class DanhMucController : ApiController
+    {
+    }
+}
---
+++ WebApplication4/Controller/SanPhamController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApplication4.Controller
+{
+    public class SanPhamController : ApiController
+    {
+        public string connection = System.Configuration.ConfigurationManager.ConnectionStrings["TESTConnectionString"].ConnectionString;
+        [HttpGet]
+        public List<SanPham> laytoanbosanpham()
+        {
+            CSDLTestDataContext context = new CSDLTestDataContext(connection);
+            List<SanPham> ds = context.SanPhams.ToList();
+            foreach (SanPham s in ds)
+            {
+                s.MaDanhMuc = null;
+            }
+            return ds;
+        }
+        [HttpGet]
+        public SanPham SanPham(int id)
+        {
+            CSDLTestDataContext context = new CSDLTestDataContext(connection);
+            SanPham sanPham = context.SanPhams.FirstOrDefault(x=> x.MaDanhMuc == id);
+            if (sanPham != null) sanPham.MaDanhMuc = null;
+            return sanPham;
+        }
+    }
+}
